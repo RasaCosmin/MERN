dbd731ffb1920b48730fe3810270a96ad973ec32
Rasa Cosmin <rasa.cosmin@gmail.com>
2018-07-12 15:03:37 +0300

    create & delete profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from "./types";
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS,
+  SET_CURRENT_USER
+} from "./types";
 
 //get current profile
 export const getCurrentProfile = () => dispach => {
@@ -18,6 +24,39 @@ export const getCurrentProfile = () => dispach => {
         payload: {}
       })
     );
+};
+
+//create profile
+export const createProfile = (profileData, history) => dispach => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispach({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//delete current account and profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
+    axios
+      .delete("/api/profile")
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.errors
+        })
+      );
+  }
 };
 
 //profile loading
